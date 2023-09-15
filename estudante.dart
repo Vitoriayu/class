957550a075d2d8db4570f84aeb506047859ba67e
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-09-15 09:59:31 -0300

    mjij

---
+++ estudante.dart
@@ -0,0 +1,20 @@
+class Estudante{
+  String nome;
+  String curso;
+  int anoDeIngresso; 
+
+   Estudante(this.nome, this.curso, this.anoDeIngresso);
+  Estudante.novoIngresso(this.nome, this.curso,)  : anoDeIngresso = DateTime.now().year;
+ 
+}
+ 
+ void main(){
+  Estudante victoria = Estudante("Victoria", "Farmacia", 2020);
+  print('Nome: ${victoria.nome}, Curso: ${victoria.curso}, Ano de Ingresso: ${victoria.anoDeIngresso}' );
+
+  Estudante fernanda = Estudante.novoIngresso("Maria", "arquitetura");
+  print('Nome: ${fernanda.nome}, Curso: ${fernanda.curso}, Ano de Ingresso: ${fernanda.anoDeIngresso}' );
+
+
+ }
+
